153b7dd71b08bb6624241107b30992d445f1bdff
Jerred Cook <jerred.cook@gmail.com>
2018-11-02 11:55:31 -0400

    further refinement with mentions

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -3,7 +3,9 @@ require 'test_helper'
 class MessageTest < ActiveSupport::TestCase
 
   def setup
+    @user = users(:example)
     @message = users(:example).messages.build(content: "Lorem ipsum")
+    @message_with_at = @user.messages.build(content: "Hi @example")
   end
 
   test "should be valid" do
@@ -13,5 +15,16 @@ class MessageTest < ActiveSupport::TestCase
   test "should not be blank" do
     @message.content = " "
     assert !@message.valid?
+  end
+
+
+  test "mentions should work" do
+    mention = @message_with_at.mentions
+    assert_not mention.empty?
+  end
+  
+  test "mentions should not work" do
+    message = @message.mentions
+    assert message.empty?
   end
 end
